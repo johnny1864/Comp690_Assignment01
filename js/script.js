fadf8f633646185c91ed36cd9d80fd781c9b54a3
unknown <j.rivera@powerdigitalmarketing.com>
2024-02-21 15:27:58 -0800

    finished step 4

--- js/script.js
+++ js/script.js
@@ -6,8 +6,20 @@ let summerMonth;
 let MyLibraryFunction;
 
 // STEP 2
- 
+
+3.9 // numeric literal
+'Hello' // string literal
+false // Boolean literal
+null // literal null value
+
+
 
 // STEP 3
 
+const myFirstExpression = 7 + 7;
+const myName = "Johnny" + " " + "Rivera";
 
+
+// STEP 4
+
+let strFirstName, strLastName, address, city, state, zipCode, intYourAge, referralSource, mayWeContactYou;
