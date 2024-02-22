9791a03bfa8b274866489feed43e28897e40be83
unknown <j.rivera@powerdigitalmarketing.com>
2024-02-21 17:18:57 -0800

    finished step 17

--- js/script.js
+++ js/script.js
@@ -52,3 +52,56 @@ let varTwo = "string 1 " + false;
 console.log(varOne);
 console.log(varTwo);
 
+// STEP 8 
+
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+console.log(someString);
+
+// STEP 9
+
+let nullVar = null;
+console.log(nullVar);
+
+let undefinedVar;
+console.log(undefinedVar);
+
+// STEP 10
+
+console.log(typeof 'Hello'); // String
+console.log(typeof 5); // Number
+console.log(typeof false); // Boolean 
+console.log(typeof null); // Object
+console.log(typeof undefinedVar); // Undefined
+
+// STEP 11
+
+alert("Hello " + "Johnny" + ", welcome to the JavaScript class!"); 
+
+// STEP 12
+
+let name = "Johnny";
+alert("Hello " + Name + ", welcome to the JavaScript class!"); 
+
+// STEP 13
+
+let name = "Johnny";
+let course = "JavaScript";
+alert("Hello " + Name + ", welcome to the " + course + " class!"); 
+
+// STEP 14
+
+let name = "Johnny";
+let course = "JavaScript";
+alert("Hello " + Name + ". \nWelcome to the " + course + " class!"); 
+
+// STEP 15 
+
+let name = prompt("Please enter your name");
+
+// STEP 16
+
+let course = prompt("What class are you taking");
+
+
+
+
