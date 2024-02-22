792092575d67f609e86acc9a3839dd49843002a0
unknown <j.rivera@powerdigitalmarketing.com>
2024-02-21 16:03:51 -0800

    finished step 7

--- js/script.js
+++ js/script.js
@@ -23,3 +23,32 @@ const myName = "Johnny" + " " + "Rivera";
 // STEP 4
 
 let strFirstName, strLastName, address, city, state, zipCode, intYourAge, referralSource, mayWeContactYou;
+
+// STEP 5
+
+let strFirstName;
+let intYourAge;
+
+strFirstName = "Johnny";
+intYourAge = 50;
+
+let strFirstName = "Johnny";
+let intYourAge = 50;
+
+let strFirstName = "Johnny", intYourAge = 50
+
+// STEP 6
+
+let str = "String var";
+let num = 20;
+
+console.log(str + num);
+
+// STEP 7 
+
+let varOne = true + " string 1";
+let varTwo = "string 1 " + false;
+
+console.log(varOne);
+console.log(varTwo);
+
